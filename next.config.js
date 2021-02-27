7184537bc8c9593816b26aee22841324f606e994
amazingshellyyy <amazingshellyyy@gmail.com>
2021-02-26 21:09:52 -0800

    "deploy"

--- next.config.js
+++ next.config.js
@@ -2,10 +2,14 @@ const pathPrefix = process.env.NODE_ENV === 'production'
     ? '/next-fcs'
     : '';
 
-module.exports = {
+// NOTE: see https://github.com/vercel/next.js/issues/7625
+const withTM = require('next-transpile-modules');
+ 
+module.exports = withTM({
+  transpileModules: ['debug'],
   assetPrefix: pathPrefix,
   basePath: pathPrefix,
   env: {
     pathPrefix,
   },
-};
+});
