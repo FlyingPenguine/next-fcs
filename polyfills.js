dfe0f95cb9e49cc5c8a9f2d5a8b71117f3435908
amazingshellyyy <amazingshellyyy@gmail.com>
2021-02-26 21:33:12 -0800

    "deploy"

--- polyfills.js
+++ polyfills.js
@@ -1,8 +1,8 @@
 /* eslint no-extend-native: 0 */
 // core-js comes with Next.js. So, you can import it like below
-import includes from 'core-js/library/es/string/virtual/includes';
-import repeat from 'core-js/library/es/string/virtual/repeat';
-import assign from 'core-js/library/es/object/assign';
+import includes from 'core-js/library/fn/string/virtual/includes';
+import repeat from 'core-js/library/fn/string/virtual/repeat';
+import assign from 'core-js/library/fn/object/assign';
 
 // Add your polyfills (from IE10 is supported by default)
 // This files runs at the very beginning (even before React and Next.js core)
