d339d04e3b9c8785b083854afb5b1742c81a56ed
amazingshellyyy <amazingshellyyy@gmail.com>
2021-02-26 20:11:06 -0800

    deploy

---
+++ docs/_next/static/hAwSnSmJcnxUNy7KCkdKv/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fc50ddf22b716c7b6b76d.css","static\u002Fchunks\u002Fpages\u002Findex-c06a82ec62d7297e7dd4.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-d6f3242283faf061b66a.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js! 1</a>
+          Welcome to <a href="https://nextjs.org">Next.js! 2</a>
         </h1>
         <form>
           <input type='file' onChange={(e) => acceptFile(e.target.files[0])} />
