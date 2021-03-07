3c0fbb9c09a1996d5e2f60f4061bd57a4feb6a79
amazingshellyyy <amazingshellyyy@gmail.com>
2021-03-06 21:24:46 -0800

    "deploy"

--- next.config.js
+++ next.config.js
@@ -3,31 +3,17 @@ const pathPrefix = process.env.NODE_ENV === 'production'
     : '';
 
 // NOTE: see https://github.com/vercel/next.js/issues/7625
-const withTM = require('next-transpile-modules')(['fcs', 'react', 'next']);
- 
+const withTM = require('next-transpile-modules')
+
 module.exports = withTM({
   assetPrefix: pathPrefix,
   basePath: pathPrefix,
   env: {
-    pathPrefix,
+    pathPrefix
   },
-  webpack: (config, {
-    isServer,
-    defaultLoaders
-  }) => {
-    const originalEntry = config.entry;
-    config.entry = async() => {
-      const entries = await originalEntry();
-
-      if (
-        entries['main.js'] &&
-        !entries['main.js'].includes('./polyfills.js')
-      ) {
-        entries['main.js'].unshift('./polyfills.js');
-      }
-
-      return entries
+  config: {
+    optimization: {
+      minimize: false
     }
-    return config
   }
 })
